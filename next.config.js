2d799339fc6113dc4d0a4068bb746b5798af269c
Ali Gümüş Tosun <=>
2023-08-30 01:02:24 +0300

    .

--- next.config.js
+++ next.config.js
@@ -6,13 +6,4 @@ module.exports = {
 		SUPABASE_URL: process.env.SUPABASE_URL,
 		SUPABASE_KEY: process.env.SUPABASE_KEY,
 	},
-	async redirects() {
-		return [
-			{
-				source: '/',
-				destination: '/dashboard',
-				permanent: true,
-			},
-		];
-	},
 };
