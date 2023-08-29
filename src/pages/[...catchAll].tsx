5b9cb99f8b078357bd3b95e328d8b74026ef9ea4
Ali Gümüş Tosun <=>
2023-08-29 20:38:23 +0300

    trying

--- src/pages/[...catchAll].tsx
+++ src/pages/[...catchAll].tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { ErrorComponent } from '@refinedev/mui';
 import { GetServerSideProps } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]';
 
 export default function CatchAll() {
 	return <ErrorComponent />;
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps<object> = async (
+	context
+) => {
+	const session = await getServerSession(
+		context.req,
+		context.res,
+		authOptions
+	);
+
+	if (session) {
+		return {
+			props: {},
+			redirect: {
+				destination: '/dashboard',
+				permanent: false,
+			},
+		};
+	}
+
 	return {
 		props: {},
 	};
