5a346ad6ca2a47cab026c6b0f36b986764eff912
Ali Gümüş Tosun <=>
2023-08-31 15:41:26 +0300

    send one cookie

--- src/pages/api/setAuthForSite.js
+++ src/pages/api/setAuthForSite.js
@@ -1,37 +1,23 @@
-import querystring from 'querystring';
-
-const cookieArray = [
-	'next-auth.session-token',
-	'next-auth.callback-url',
-	'next-auth.csrf-token',
-	'__Secure-next-auth.session-token',
-	'__Secure-next-auth.callback-url',
-	'__Host-next-auth.csrf-token',
-];
+function getCookieValue(cookieString, cookieName) {
+	const cookies = cookieString.split(';').map((cookie) => cookie.trim());
+	for (const cookie of cookies) {
+		const [name, value] = cookie.split('=');
+		if (name === cookieName) {
+			return decodeURIComponent(value);
+		}
+	}
+	return null; // Cookie not found
+}
 
 export default (req, res) => {
 	// get cookie value from request params
 	const cookies = req.headers.cookie || '';
 	const { site } = req.query;
-	const parsedCookies = {};
 
-	// Split the cookies and store them in the parsedCookies object
-	cookies.split(';').forEach((cookie) => {
-		const [key, value] = cookie.split('=');
-		parsedCookies[key.trim()] = value;
-	});
-
-	// Filter cookies by the ones you specified in your cookieArray
-	const filteredCookies = {};
-	for (const key of cookieArray) {
-		if (parsedCookies[key]) {
-			filteredCookies[key] = parsedCookies[key];
-		}
-	}
+	const cookieName = '__Secure-next-auth.session-token';
+	const cookieValue = getCookieValue(cookies, cookieName);
 
 	// Convert the filteredCookies object into a query string
-	const queryString = querystring.stringify(filteredCookies);
-	console.log(queryString);
 	// Now you have the cookies turned into a query string
-	res.redirect(`${site}/api/setCookies?${queryString}`);
+	res.redirect(`${site}/api/setCookies?cookieValue=${cookieValue}}`);
 };
--- src/pages/api/setCookies.js
+++ src/pages/api/setCookies.js
@@ -2,18 +2,17 @@ import { serialize } from 'cookie';
 
 export default (req, res) => {
 	// get cookie value from request params
-	const cookies = req.query;
-	console.log(Object.keys(cookies));
-	let cookieString = '';
-	for (const key of Object.keys(cookies)) {
-		// encode the value of the cookie
-		cookieString += serialize(key, cookies[key], {
+	const { cookieValue } = req.query;
+	const cookieString = serialize(
+		'__Secure-next-auth.session-token',
+		encodeURIComponent(cookieValue),
+		{
 			httpOnly: true,
 			secure: true,
 			sameSite: 'none', // Adjust this if needed based on your requirements
 			path: '/', // Adjust the path attribute if needed
-		});
-	}
+		}
+	);
 
 	res.setHeader('Set-Cookie', cookieString);
 
