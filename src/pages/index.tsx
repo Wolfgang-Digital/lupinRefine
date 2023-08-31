bedb30cf28caa284cc34e24bed50cdccdf97c2ad
Ali Gümüş Tosun <=>
2023-08-31 12:11:30 +0300

    index routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 export default function Index() {
+	const router = useRouter();
+	useEffect(() => {
+		router.push('/dashboard');
+	}, []);
 	return <>index</>;
 }
 
