deb76223c116c999a004835302293c276c1fed94
Ali Gümüş Tosun <=>
2023-08-31 14:40:25 +0300

    auth

---
+++ src/pages/api/setCookies.js
@@ -0,0 +1,21 @@
+import { serialize } from 'cookie';
+
+export default (req, res) => {
+	// get cookie value from request params
+	const cookies = req.query;
+	console.log(Object.keys(cookies));
+	let cookieString = '';
+	for (const key of Object.keys(cookies)) {
+		cookieString += serialize(key, cookies[key], {
+			httpOnly: true,
+			secure: true,
+			sameSite: 'lax', // Adjust this if needed based on your requirements
+			path: '/', // Adjust the path attribute if needed
+		});
+	}
+
+	res.setHeader('Set-Cookie', cookieString);
+
+	// Now, the query parameters have been turned into cookies with their values
+	res.status(200).send('Cookies set successfully');
+};
