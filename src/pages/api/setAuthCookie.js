0fc652bd1e88d96aad3fc072a2fff898d9a38b44
Ali Gümüş Tosun <=>
2023-08-31 13:27:54 +0300

    secure

--- src/pages/api/setAuthCookie.js
+++ src/pages/api/setAuthCookie.js
@@ -1,14 +1,12 @@
 export default (req, res) => {
 	// get cookie value from request params
 	const { cookie } = req.query;
-	console.log(req.query);
 	res.setHeader(
 		'Set-Cookie',
-		`__Secure-next-auth.session-token=${cookie}; HttpOnly; Path=/; Expires=${new Date(
+		`__Secure-next-auth.session-token=${cookie}; HttpOnly; Secure; Path=/; Expires=${new Date(
 			Date.now() + 7 * 24 * 60 * 60 * 1000
 		).toUTCString()}`
 	);
 
-	// redirect to dashboard
 	res.status(200).json({ message: 'Cookie set!' });
 };
