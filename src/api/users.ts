09417d45e4c2dac346fd4589dd3849b9f3d883d4
polobryn <polobryn@yahoo.co.uk>
2023-09-08 14:14:11 +0100

    Fix db query for client.ts and users.ts api files

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,17 +1,17 @@
-import supabase from '@config/supaBaseClient';
+import supabase from "@config/supaBaseClient";
 
 export const getAllUsers = async () => {
-	try {
-		const { data, error } = await supabase
-			.from('wolfgangers')
-			.select('*')
-			.order('user_id', { ascending: true });
-		if (error) {
-			console.error('Error fetching clients:', error);
-			return;
-		}
-		return data;
-	} catch (error) {
-		console.error('Error fetching clients:', error);
-	}
+  try {
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")
+      .order("user_id", { ascending: true });
+    if (error) {
+      console.error("Error fetching clients:", error);
+      return;
+    }
+    return data;
+  } catch (error) {
+    console.error("Error fetching clients:", error);
+  }
 };
