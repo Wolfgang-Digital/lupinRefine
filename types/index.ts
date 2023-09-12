6675a47fb7b42b63347cac410ea77e1556da23ad
polobryn <polobryn@yahoo.co.uk>
2023-09-12 10:48:58 +0100

    Resolve nextauth.ts and wolfganger issue

--- types/index.ts
+++ types/index.ts
@@ -3,10 +3,9 @@ export type Tables = Database["public"]["Tables"];
 export type Views = Database["public"]["Views"];
 export type Client = Tables["client"]["Row"];
 export type Currency = Tables["currency"]["Row"];
-export type Wolfgangers = Tables["wolfgangers"]["Row"];
+export type Wolfgangers = Tables["users"]["Row"];
 export type Tier = Tables["tier"]["Row"];
 export type Job = Tables["jobs"]["Row"];
 export type Country = Tables["countries"]["Row"];
-export type FinancialTable = Views["timesheet_rows_byuser_v4"]["Row"];
-export type JobsDropdownView =
-	Views["timesheet_jobsresponse_dropdown"]["Row"];
+export type FinancialTable = Views["timesheet_rows_view"]["Row"];
+export type JobsDropdownView = Views["timesheet_jobsresponse_dropdown"]["Row"];
