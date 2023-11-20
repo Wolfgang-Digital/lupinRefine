3d158054167c0fce885640079c109eee702522ad
polobryn <polobryn@yahoo.co.uk>
2023-11-20 09:42:16 +0000

    Remove unused var - lint

--- src/pages/api/allocateHoursView.ts
+++ src/pages/api/allocateHoursView.ts
@@ -47,8 +47,8 @@ export const getUserAllocatedHoursPerMonth = async (
 };
 
 export const getJobAllocatedHoursPerMonth = async (
-	jobID: number,
-	clientID: number
+	jobID: number
+	// clientID: number
 	// month: number
 ) => {
 	try {
