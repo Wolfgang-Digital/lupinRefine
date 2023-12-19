d9596d49fa3bfa976c8e7713e8338ef76923dead
polobryn <polobryn@yahoo.co.uk>
2023-12-19 15:58:22 +0000

    Fix jobName for new job on financials page

--- src/pages/api/jobNames.ts
+++ src/pages/api/jobNames.ts
@@ -21,3 +21,24 @@ export const getJobNames = async (): Promise<AllJobNames[] | undefined> => {
 		console.error("Error fetching Job Names data: ", error);
 	}
 };
+
+export const getJobName = async (
+	jobNameId: number
+): Promise<AllJobNames[] | undefined> => {
+	try {
+		const { data, error } = (await supabase
+			.from("job_names")
+			.select("job_name_name")
+			.eq("job_name_id", jobNameId)) as unknown as {
+			data: AllJobNames[];
+			error: PostgrestError;
+		};
+		if (error) {
+			console.error("Error fetching Job Name data: ", error);
+			return;
+		}
+		return data;
+	} catch (error) {
+		console.error("Error fetching Job Name data: ", error);
+	}
+};
